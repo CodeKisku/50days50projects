9063288fac2dee22959d24286b4fe15d3fb4d227
Sanatan Kisku <sanatankisku@gmail.com>
2023-07-08 21:18:35 +0530

    Add Eventlistener for nav

--- Rotating Navigation/script.js
+++ Rotating Navigation/script.js
@@ -2,4 +2,10 @@ const open = document.getElementById('open')
 const close = document.getElementById('close')
 const container = document.querySelector('.container')
 
-open.addEventLister('click')
+open.addEventListener('click', () => {
+  container.classList.add('show-nav')
+})
+
+close.addEventListener('click', () => {
+  container.classList.remove('show-nav')
+})
