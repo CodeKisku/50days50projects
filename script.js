e489da3329e6b840232d57e08d57f4948dd9c3ae
Sanatan Kisku <sanatankisku@gmail.com>
2023-07-23 11:07:54 +0530

    Add search functionality

--- script.js
+++ script.js
@@ -6,3 +6,26 @@ btn.addEventListener('click', () => {
   search.classList.toggle('active')
   input.focus()
 })
+
+const searchtype = () => {
+  const searchbox = document.getElementById('input').value.toUpperCase();
+  const projectItem = document.getElementById('projects-container');
+  const project = document.querySelectorAll('.project')
+  const pname = document.querySelectorAll('.project-title')
+
+  for (var i = 0; i < pname.length; i++) {
+    let match = project[i].querySelectorAll('.project-title')[0];
+
+    if (match) {
+      let textValue = match.textContent || match.innerHTML
+
+      if (textValue.toUpperCase().indexOf(searchbox) > -1) {
+        project[i].style.display = "";
+      } else {
+        project[i].style.display = "none";
+      }
+    }
+
+  }
+
+}
